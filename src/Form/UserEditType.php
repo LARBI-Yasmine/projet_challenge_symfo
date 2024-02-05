66cafcba02a949415853af670983e61cf39044b5
l3miage-larbiy <larbi1992yasmine@gmail.com>
2024-02-05 12:13:34 +0100

    Adding edit profile FORM for all users

---
+++ src/Form/UserEditType.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('firstname',TextType::class, [
+            'label' => 'Prénom',
+            'required' => true,
+            'attr' => [
+                'placeholder' => "Votre nouveau prénom",
+                
+            ],
+            ])
+        ->add('lastname',TextType::class, [
+            'label' => 'Nom',
+            'required' => true,
+            'attr' => [
+                'placeholder' => "Votre nouveau nom",
+                
+            ],
+            ])
+        ->add('email',EmailType::class) ;   
+        
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
